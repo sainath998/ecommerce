acbbf04ab567be4ecb2b0661801141ee5b046e56
sainath <sainathreddy863@gmail.com>
2022-03-23 10:49:13 +0530

    added all files

---
+++ frontend/src/component/layout/Contact/Contact.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Contact.css";
+import { Button } from "@material-ui/core";
+
+const Contact = () => {
+  return (
+    <div className="contactContainer">
+      <a className="mailBtn" href="mailto:sainathreddy863@gmail.com">
+        <Button>Contact: sainathreddy863@gmail.com</Button>
+        
+      </a>
+    </div>
+  );
+};
+
+export default Contact;
